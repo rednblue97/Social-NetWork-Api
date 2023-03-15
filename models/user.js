ab57a62d0e53f9ec9b913aa986141f54091d4fe6
CaptureB <airdavid62@gmail.com>
2023-03-15 12:11:51 -0600

    Finished the user models

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /.\@.+\..+/,
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'thoughts',
+  },
+],
+},
+{
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+}
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const user = model('user', userSchema);
+
+module.exports = user
