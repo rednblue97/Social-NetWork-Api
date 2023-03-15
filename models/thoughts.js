c524c1b8aef011112084fb9ef64ca140085975f6
CaptureB <airdavid62@gmail.com>
2023-03-15 11:53:44 -0600

    completed the thoughts.js models

---
+++ models/thoughts.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./reaction');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        max_length: 280,
+    },
+    createdAt: {
+        type:Date,
+        default: Date.now,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [
+        reactionSchema
+    ],
+},
+{
+    toJSON: {
+        virtuals: true,
+    },
+    id: false,
+}
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+const thoughts = model('thoughts', thoughtSchema);
+
+module.exports = thoughts
