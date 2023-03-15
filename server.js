ccd907967f418033e655ef58b8a1704da602c691
CaptureB <airdavid62@gmail.com>
2023-03-15 01:21:12 -0600

    finished the server.js to run application

---
+++ server.js
@@ -0,0 +1,18 @@
+// importing express establish routes and connection
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+// port running localhost
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on port ${PORT}!`);
+  });
+});
